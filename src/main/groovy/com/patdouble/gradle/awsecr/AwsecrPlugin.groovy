f8fa08c912178c6c10aacd691f19795c74eb8cd2
Patrick Double <pat@patdouble.com>
2016-11-07 07:39:05 -0600

    Initial impl taken from original private repo code

---
+++ src/main/groovy/com/patdouble/gradle/awsecr/AwsecrPlugin.groovy
@@ -0,0 +1,115 @@
+package com.patdouble.gradle.awsecr
+
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.ecr.AmazonECRClient
+import com.amazonaws.services.ecr.model.GetAuthorizationTokenRequest
+import com.amazonaws.services.ecr.model.GetAuthorizationTokenResult
+import com.bmuschko.gradle.docker.DockerRegistryCredentials
+import com.bmuschko.gradle.docker.tasks.RegistryCredentialsAware
+import org.gradle.api.GradleException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+import java.util.regex.Pattern
+
+class AwsecrPlugin implements Plugin<Project> {
+    private static final Pattern AWS_ECR_URL = ~/https:\/\/([0-9A-Za-z]+)\.dkr\.ecr\.[a-zA-Z0-9-]+\.amazonaws\.com/
+
+    Project project
+
+    @Override
+    void apply(Project project) {
+        assert project
+        this.project = project
+
+        project.afterEvaluate {
+            project.tasks.withType(RegistryCredentialsAware) { task ->
+                configureRegistryCredentials(task)
+            }
+        }
+    }
+
+    protected String findRepository(RegistryCredentialsAware registryCredentialsAware) {
+        if (registryCredentialsAware.hasProperty('repository')) {
+            return registryCredentialsAware['repository'] as String
+        } else if (registryCredentialsAware.hasProperty('imageName')) {
+            return registryCredentialsAware['imageName'] as String
+        } else if (registryCredentialsAware.hasProperty('tag')) {
+            return registryCredentialsAware['tag'] as String
+        }
+        project.logger.info("Skipping docker credentials for ${registryCredentialsAware} because we do not know how to find a repository for this type (${registryCredentialsAware.class}")
+        null
+    }
+
+    /**
+     * Extract the ECR repository ID and URL
+     * @return map containing 'id' and 'url' keys or empty if repository URL is unknown
+     */
+    protected Map<String, String> extractEcrInfo(CharSequence repository) {
+        def m = AWS_ECR_URL.matcher(repository)
+        if (!m) {
+            return [:]
+        }
+
+        ['id' : m.group(1),
+         'url': m.group(0)]
+    }
+
+    protected void configureRegistryCredentials(RegistryCredentialsAware registryCredentialsAware) {
+        String repository = findRepository(registryCredentialsAware)
+        if (!repository) {
+            return
+        }
+
+        def info = extractEcrInfo(repository)
+        if (!info) {
+            project.logger.info("Skipping docker credentials for ${registryCredentialsAware} because repository '${repository}' is not AWS ECR")
+            return
+        }
+
+        String registryId = info.id
+        String registryUrl = info.url
+        project.logger.info("Found ECR registry account ID ${registryId} at ${registryUrl}")
+        registryCredentialsAware.registryCredentials = findEcrCredentials(registryId, registryUrl)
+    }
+
+    /**
+     * Determine the AWS ECR credentials, if present.
+     * @return
+     */
+    protected DockerRegistryCredentials findEcrCredentials(String registryId, String registryUrl) {
+        def awsAccessKeyId = System.getenv('AWS_ACCESS_KEY_ID')
+        def awsSecretAccessKey = System.getenv('AWS_SECRET_ACCESS_KEY')
+        if (!awsAccessKeyId || !awsSecretAccessKey) {
+            throw new GradleException("AWS ECR registry requires AWS account credentials configured in environment AWS_ACCESS_KEY_ID/AWS_SECRET_ACCESS_KEY")
+        }
+
+        def credFile = project.rootProject.file(".gradle/ecr${Math.abs((awsAccessKeyId + awsSecretAccessKey + registryId).hashCode())}.properties")
+
+        if (credFile.canRead() && credFile.isFile()) {
+            Properties props = new Properties()
+            credFile.withInputStream { props.load(it) }
+            if (props.get('expiresAt') && Long.parseLong(props.getProperty('expiresAt')) > (System.currentTimeMillis() + 3600000)) {
+                project.logger.info("Using ECR credentials from ${credFile}")
+                return new DockerRegistryCredentials(url: registryUrl, username: props.getProperty('username'), password: props.getProperty('password'))
+            }
+        }
+
+        AmazonECRClient ecrClient = new AmazonECRClient(new BasicAWSCredentials(awsAccessKeyId, awsSecretAccessKey))
+        GetAuthorizationTokenResult tokens = ecrClient.getAuthorizationToken(new GetAuthorizationTokenRequest().withRegistryIds(registryId))
+        if (tokens.authorizationData) {
+            def ecrCreds = new String(tokens.authorizationData.first().authorizationToken.decodeBase64(), 'US-ASCII').split(':')
+
+            Properties props = new Properties()
+            props.setProperty('username', ecrCreds[0])
+            props.setProperty('password', ecrCreds[1])
+            props.setProperty('expiresAt', String.valueOf(tokens.authorizationData.first().expiresAt.time))
+            credFile.parentFile.mkdirs()
+            credFile.withOutputStream { props.store(it, "ECR Credentials for ${awsAccessKeyId} @ ${registryUrl}") }
+
+            return new DockerRegistryCredentials(url: registryUrl, username: ecrCreds[0], password: ecrCreds[1])
+        }
+
+        throw new GradleException("Could not get ECR token: ${tokens.sdkHttpMetadata.httpStatusCode}")
+    }
+}
--- src/test/groovy/com/patdouble/AwsecrPluginSpec.groovy
+++ src/test/groovy/com/patdouble/AwsecrPluginSpec.groovy
@@ -1,21 +1,97 @@
 package com.patdouble
 
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
+import com.bmuschko.gradle.docker.tasks.image.DockerPushImage
+import com.patdouble.gradle.awsecr.AwsecrPlugin
+import org.gradle.api.GradleException
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
 class AwsecrPluginSpec extends Specification {
 
+
+    public static final String FAKE_REPO = 'https://123456789.dkr.ecr.us-east-1.amazonaws.com'
+
     def "apply() should load the plugin"() {
         given:
         def project = ProjectBuilder.builder().build()
-
         when:
         project.with {
             apply plugin: 'com.patdouble.awsecr'
         }
-
+        project.evaluate()
         then:
         project.plugins.hasPlugin(AwsecrPlugin)
     }
 
+    def "findRepository() for DockerPullImage"() {
+        given:
+        def project = ProjectBuilder.builder().build()
+        def plugin = new AwsecrPlugin()
+        def task = project.tasks.create('pull', DockerPullImage) {
+            repository = FAKE_REPO
+        }
+        expect:
+        plugin.findRepository(task) == FAKE_REPO
+    }
+
+    def "findRepository() for DockerBuildImage"() {
+        given:
+        def fakeTag = FAKE_REPO+'/myimage:latest'
+        def project = ProjectBuilder.builder().build()
+        def plugin = new AwsecrPlugin()
+        def task = project.tasks.create('build', DockerBuildImage) {
+            tag = fakeTag
+        }
+        expect:
+        plugin.findRepository(task) == fakeTag
+    }
+
+    def "findRepository() for DockerPushImage"() {
+        given:
+        def fakeImage = FAKE_REPO+'/myimage'
+        def project = ProjectBuilder.builder().build()
+        def plugin = new AwsecrPlugin()
+        def task = project.tasks.create('push', DockerPushImage) {
+            imageName = fakeImage
+        }
+        expect:
+        plugin.findRepository(task) == fakeImage
+    }
+
+    def "extractEcrInfo"() {
+        given:
+        def plugin = new AwsecrPlugin()
+        def info = ['id':'123456789', 'url': FAKE_REPO]
+        expect:
+        plugin.extractEcrInfo(FAKE_REPO) == info
+        plugin.extractEcrInfo(FAKE_REPO+'/myimage') == info
+        plugin.extractEcrInfo(FAKE_REPO+'/myimage:latest') == info
+        plugin.extractEcrInfo('').isEmpty()
+        plugin.extractEcrInfo('library/mongo').isEmpty()
+    }
+
+    def "registry aware tasks are found"() {
+        given:
+        def project = ProjectBuilder.builder().build()
+        project.with {
+            apply plugin: 'com.patdouble.awsecr'
+            tasks.create('pull', DockerPullImage) {
+                repository = FAKE_REPO
+            }
+            tasks.create('build', DockerBuildImage) {
+                tag = FAKE_REPO+'/myimage:latest'
+            }
+            tasks.create('push', DockerPushImage) {
+                imageName = FAKE_REPO+'/myimage'
+            }
+        }
+
+        when:
+        project.evaluate()
+
+        then:
+        thrown(GradleException)
+    }
 }
